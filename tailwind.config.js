843c2e50094617022b06ca066f98e0c6bb63525f
Santosh kumar gupta <sainkee1997@gmail.com>
2024-02-05 10:53:45 +0530

    footer list  added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,18 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
-      boxShadow:{
-        mid:" rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+      gridTemplateCol: {
+        six: "repeat(auto-fit, minmax(6rem, auto))",
       },
-      
+      boxShadow: {
+        mid: " rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+      },
+
       colors: {
         primary: "#ff6e61",
-        offwhite :"#f6f6f6",
-        dull :"#ffffff",
-        beige :"#fffcf8"
+        offwhite: "#f6f6f6",
+        dull: "#ffffff",
+        beige: "#fffcf8",
       },
     },
   },
