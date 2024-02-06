f3121257e95caae54d7786b69b6b51658d8a25ea
Santosh kumar gupta <sainkee1997@gmail.com>
2024-02-06 10:46:48 +0530

    slider added

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,26 @@ module.exports = {
       boxShadow: {
         mid: " rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
       },
+      lineHeight: {
+        custom: "1.42857143",
+      },
+
+      fontFamily: {
+        sans: [
+          "Clear Sans",
+          "Helvetica Neue",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+        ],
+      },
 
       colors: {
         primary: "#ff6e61",
         offwhite: "#f6f6f6",
         dull: "#ffffff",
         beige: "#fffcf8",
+        safe: "#ffe3c2",
       },
     },
   },
